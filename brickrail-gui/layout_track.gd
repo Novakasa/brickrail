977c3d7982dd09fa6a80ac711b1a629d99566653
Novakasa <lolligerjoj@gmail.com>
2021-03-31 23:29:58 +0200

    alternative fields for layout_track

--- brickrail-gui/layout_track.gd
+++ brickrail-gui/layout_track.gd
@@ -1,28 +1,31 @@
 class_name LayoutTrack
 extends Node2D
 
-var slots
-var connections
-var valid_slots = ["N", "S", "E", "W"]
+var slot0
+var slot1
+var connections0 = []
+var connections1 = []
+var slots = ["N", "S", "E", "W"]
 
 func _init(p_slot0, p_slot1):
-	assert(p_slot0 in valid_slots and p_slot1 in valid_slots)
-	assert(p_slot0 != p_slot1)
-	slots = {0: p_slot0, 1: p_slot1}
+	slot0 = p_slot0
+	slot1 = p_slot1
+	assert(slot0 != slot1)
+	assert(slot0 in slots and slot1 in slots)
 
 func get_opposite_slot(slot):
-	if slots[0] == slot:
-		return slot[1]
-	if slots[1] == slot:
-		return slot[0]
+	if slot0 == slot:
+		return slot1
+	elif slot1 == slot:
+		return slot0
 	push_error("[LayoutTrack.get_opposite_slot] track doesn't contain " + slot)
 
 func get_next_tracks_from(slot):
 	return get_next_tracks_at(get_opposite_slot(slot))
 	
 func get_next_tracks_at(slot):
-	if slot == slot[0]:
-		return connections[0]
-	elif slot == slot[1]:
-		return connections[1]
+	if slot == slot0:
+		return connections0
+	elif slot == slot1:
+		return connections1
 	push_error("[LayoutTrack.get_next_tracks_at] track doesn't contain " + slot)
