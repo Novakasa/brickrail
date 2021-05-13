84d5558ecfa3ae1fe90a111a08b18166e97f97e4
Novakasa <lolligerjoj@gmail.com>
2021-05-13 18:24:01 +0200

    move hub related stuff out of train

--- brickrail-gui/ble_train.gd
+++ brickrail-gui/ble_train.gd
@@ -19,17 +19,11 @@ signal mode_changed(mode)
 signal slow_marker_changed(marker)
 signal stop_marker_changed(marker)
 signal name_changed(old_name, new_name)
-signal connected
-signal disconnected
-signal connect_error(data)
 
 func _init(p_name, p_address):
 	name = p_name
 	hub = BLEHub.new(p_name, "train", p_address)
 	hub.connect("data_received", self, "_on_data_received")
-	hub.connect("connected", self, "_on_hub_connected")
-	hub.connect("disconnected", self, "_on_hub_disconnected")
-	hub.connect("connect_error", self, "_on_hub_connect_error")
 
 func _on_data_received(key, data):
 	prints("train received:", key, data)
@@ -45,15 +39,6 @@ func _on_data_received(key, data):
 		stop_marker = data
 		emit_signal("stop_marker_changed", data)
 
-func _on_hub_connected():
-	emit_signal("connected")
-
-func _on_hub_disconnected():
-	emit_signal("disconnected")
-
-func _on_hub_connect_error(data):
-	emit_signal("connect_error", data)
-
 func set_state(p_state):
 	state = p_state
 	emit_signal("state_changed", state)
@@ -64,9 +49,6 @@ func set_name(p_new_name):
 	hub.set_name(p_new_name)
 	emit_signal("name_changed", old_name, p_new_name)
 
-func set_address(p_address):
-	hub.set_address(p_address)
-
 func set_target(value):
 	var cmd = "train.set_target(" + str(value) + ")"
 	hub.hub_command(cmd)
@@ -74,15 +56,6 @@ func set_target(value):
 func set_speed(value):
 	var cmd = "train.set_speed(" + str(value) + ")"
 	hub.hub_command(cmd)
-
-func connect_hub():
-	hub.connect_hub()
-
-func disconnect_hub():
-	hub.disconnect_hub()
-
-func run_program():
-	hub.run_program()
 
 func brake():
 	hub.hub_command("train.brake()")
--- brickrail-gui/train_control_gui.gd
+++ brickrail-gui/train_control_gui.gd
@@ -15,9 +15,9 @@ func setup(p_project, p_train_name):
 	project = p_project
 	set_train_name(p_train_name)
 	get_train().connect("name_changed", self, "_on_train_name_changed")
-	get_train().connect("connected", self, "_on_train_connected")
-	get_train().connect("disconnected", self, "_on_train_disconnected")
-	get_train().connect("connect_error", self, "_on_train_connect_error")
+	get_train().hub.connect("connected", self, "_on_train_connected")
+	get_train().hub.connect("disconnected", self, "_on_train_disconnected")
+	get_train().hub.connect("connect_error", self, "_on_train_connect_error")
 	get_train().hub.connect("program_started", self, "_on_program_started")
 	get_train().hub.connect("program_stopped", self, "_on_program_stopped")
 	get_train().connect("mode_changed", self, "_on_mode_changed")
@@ -125,9 +125,9 @@ func _on_connect_button_pressed():
 	var runbutton = get_node(run_button)
 	var connectbutton = get_node(connect_button)
 	if connectbutton.text == "connect":
-		get_train().connect_hub()
+		get_train().hub.connect_hub()
 	if connectbutton.text == "disconnect":
-		get_train().disconnect_hub()
+		get_train().hub.disconnect_hub()
 	connectbutton.disabled=true
 	runbutton.disabled=true
 	
