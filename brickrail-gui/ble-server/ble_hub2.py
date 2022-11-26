f3575306215361b9a5102b65054ad0c2612a979b
Johannes Ringler <johannesringler@gmail.com>
2022-11-26 17:08:44 +0100

    wip3, ready to test

--- brickrail-gui/ble-server/ble_hub2.py
+++ brickrail-gui/ble-server/ble_hub2.py
@@ -3,27 +3,92 @@ import asyncio
 from pybricksdev.ble import find_device
 from pybricksdev.connections.pybricks import PybricksHub
 
-_OUT_ID_SIGNAL = 0
-_OUT_ID_DATA = 1
-_OUT_ID_SYS = 2
-_OUT_ID_EOM = b"\r"[0]
+_IN_ID_START   = 2  #ASCII start of text
+_IN_ID_END     = 3  #ASCII end of text
+_IN_ID_MSG_ACK = 6  #ASCII ack
+_IN_ID_RPC     = 17 #ASCII device control 1
+_IN_ID_SYS     = 18 #ASCII device control 2
+_IN_ID_SIGNAL  = 19 #ASCII device control 3
+_IN_ID_MSG_ERR = 21 #ASCII nak
 
-_SYS_CODE_STOP = 0
-_SYS_CODE_READY = 1
-_SYS_CODE_MSG_ACK = 2
-_SYS_CODE_MSG_ERR = 3
+_OUT_ID_START   = 2  #ASCII start of text
+_OUT_ID_END     = 3  #ASCII end of text
+_OUT_ID_MSG_ACK = 6  #ASCII ack
+_OUT_ID_DATA    = 17 #ASCII device control 1
+_OUT_ID_SYS     = 18 #ASCII device control 2
+_OUT_ID_SIGNAL  = 19 #ASCII device control 3
+_OUT_ID_MSG_ERR = 21 #ASCII nak
+
+_CHUNK_LENGTH = 80
+
+def xor_checksum(bytes):
+    checksum = 0xFF
+    for byte in bytes:
+        checksum ^= byte
+    return checksum
 
 class BLEHub:
 
     def __init__(self):
 
         self.hub = PybricksHub()
+        self.hub.nus_observable.subscribe(self._on_nus)
+        self.msg_ack = asyncio.Queue
+        self.output_buffer = bytearray()
+        self.output_queue = asyncio.Queue
     
-    def _on_hub_message(bytes):
+    def _on_hub_nus(self, data):
+        if self.hub._downloading_via_nus:
+            return
+        
+        self.output_buffer += data
+
+        while _OUT_ID_END in self.output_buffer:
+            index = self.output_buffer.find(_OUT_ID_END)
+            self.output_queue.put_nowait(self.output_buffer[0:index])
+            del self.output_buffer[0 : index + 1]
+    
+    async def hub_message_handler(self, bytes):
         out_id = bytes[0]
+
+        if out_id == _OUT_ID_MSG_ACK:
+            await self.msg_ack.put(True)
+            return
+        if out_id == _OUT_ID_MSG_ERR:
+            await self.msg_ack.put(False)
+            return
+
+        checksum = bytes[-1]
+        if not checksum == xor_checksum(bytes[:-1]):
+            await self.send_ack(False)
+            return
+        await self.send_ack(True)
+        data = bytes[1:-1] #strip out_id and checksum
 
         if out_id == _OUT_ID_SYS:
             assert len(bytes) == 2
             sys_code = bytes[1]
-            if sys_code == _SYS_CODE_MSG_ACK:
-                
+        
+        if out_id == _OUT_ID_DATA:
+            struct  = eval(data.decode())
+        
+    async def send_bytes(self, bytes):
+        assert len(bytes) <= _CHUNK_LENGTH
+        checksum = xor_checksum(bytes)
+        ack_ok = None
+        while not ack_ok == checksum:
+            await self.hub.write(bytes + bytes([checksum, _IN_ID_END]))
+            try:
+                ack_ok = await asyncio.wait_for(self.msg_ack.get(), 1)
+            except asyncio.TimeoutError:
+                print(f"Wait for acknowledgement timed out, resending")
+            else:
+                if not ack_ok:
+                    print(f"Error received from hub, retrying")
+    
+    async def send_ack(self, success):
+        if success:
+            self.hub.write(bytes([_IN_ID_MSG_ACK, _IN_ID_END]))
+        else:
+            self.hub.write(bytes([_IN_ID_MSG_ERR, _IN_ID_END]))
+
--- brickrail-gui/ble-server/hub_programs/io_hub.py
+++ brickrail-gui/ble-server/hub_programs/io_hub.py
@@ -8,22 +8,28 @@ from pybricks.tools import wait, StopWatch
 # disable keyboard interrupt character
 micropython.kbd_intr(-1)
 
-_IN_ID_SIGNAL = const(0)
-_IN_ID_RPC = const(1)
-_IN_ID_SYS = const(2)
-_IN_ID_EOM = const(b"\r"[0])
+_IN_ID_START   = const(2)  #ASCII start of text
+_IN_ID_END     = const(3)  #ASCII end of text
+_IN_ID_MSG_ACK = const(6)  #ASCII ack
+_IN_ID_RPC     = const(17) #ASCII device control 1
+_IN_ID_SYS     = const(18) #ASCII device control 2
+_IN_ID_SIGNAL  = const(19) #ASCII device control 3
+_IN_ID_MSG_ERR = const(21) #ASCII nak
 
-_OUT_ID_SIGNAL = const(0)
-_OUT_ID_DATA = const(1)
-_OUT_ID_SYS = const(2)
-_OUT_ID_EOM = const(b"\r"[0])
+# _IN_IDS = [_IN_ID_START, _IN_ID_END, _IN_ID_MSG_ACK, _IN_ID_RPC, _IN_ID_SYS, _IN_ID_SIGNAL, _IN_ID_MSG_ERR]
+
+_OUT_ID_START   = const(2)  #ASCII start of text
+_OUT_ID_END     = const(3)  #ASCII end of text
+_OUT_ID_MSG_ACK = const(6)  #ASCII ack
+_OUT_ID_DATA    = const(17) #ASCII device control 1
+_OUT_ID_SYS     = const(18) #ASCII device control 2
+_OUT_ID_SIGNAL  = const(19) #ASCII device control 3
+_OUT_ID_MSG_ERR = const(21) #ASCII nak
 
 _SYS_CODE_STOP = const(0)
 _SYS_CODE_READY = const(1)
-_SYS_CODE_MSG_ACK = const(2)
-_SYS_CODE_MSG_ERR = const(3)
 
-_CHUNK_LENGTH = const(80)
+# _CHUNK_LENGTH = const(80)
 
 class IOHub:
 
@@ -35,35 +41,64 @@ class IOHub:
         self.poll.register(usys.stdin)
         self.device = device
     
+    def emit_msg(self, data):
+        checksum = 0xFF
+        for byte in bytes:
+            checksum ^= byte
+        data += bytes([checksum, _OUT_ID_END])
+        usys.stdout.buffer.write(data)
+    
     def emit_data(self, key, data):
-        usys.stdout.buffer.write(bytes([_OUT_ID_DATA]) + bytes(repr((key, data))) + bytes([_OUT_ID_EOM]))
+        self.emit_msg(bytes([_OUT_ID_DATA]) + bytes(repr((key, data))))
 
     def emit_signal_code(self, code):
-        usys.stdout.buffer.write(bytes([_OUT_ID_SIGNAL, code, _OUT_ID_EOM]))
+        self.emit_msg(bytes([_OUT_ID_SIGNAL, code]))
     
     def emit_sys_code(self, code):
-        usys.stdout.buffer.write(bytes([_OUT_ID_SYS, code, _OUT_ID_EOM]))
+        self.emit_msg(bytes([_OUT_ID_SYS, code]))
+    
+    def emit_ack(self, success):
+        if success:
+            usys.stdout.buffer.write(bytes([_OUT_ID_MSG_ACK, _OUT_ID_END]))
+        else:
+            usys.stdout.buffer.write(bytes([_OUT_ID_MSG_ERR, _OUT_ID_END]))
+        
 
     def handle_input(self):
-        msg_id = self.input_buffer[0]
+        in_id = self.input_buffer[0]
+
+        if in_id == _IN_ID_MSG_ACK:
+            return
+        
+        if in_id == _IN_ID_MSG_ERR:
+            return
+        
+        checksum = self.input_buffer[-1]
+        if checksum != self.input_checksum:
+            self.emit_ack(False)
+        self.emit_ack(True)
+
         msg = self.input_buffer
         del msg[0]
-        if msg_id == _IN_ID_SYS:
+        del msg[-1]
+
+        if in_id == _IN_ID_SYS:
             code = msg[0]
             if code == _SYS_CODE_STOP:
                 self.running = False
                 return
-        if msg_id == _IN_ID_RPC:
+        
+        if in_id == _IN_ID_RPC:
             try:
                 struct = eval(msg)
             except SyntaxError:
-                print("[ble_hub] Syntaxerror when running eval()")
+                print("eval syntaxerror")
                 print(msg)
             func = getattr(self.device, struct["func"])
             args = struct["args"]
             _result = func(*args)
             return
-        if msg_id == _IN_ID_SIGNAL:
+        if in_id == _IN_ID_SIGNAL:
             self.device.on_signal_received(msg)
             return
         
@@ -71,25 +106,13 @@ class IOHub:
 
     def update_input(self, byte):
 
-        if byte == _IN_ID_EOM:
+        if byte == _IN_ID_END:
             self.handle_input()
             self.input_buffer = bytearray()
-            return
-        if len(self.input_buffer)%_CHUNK_LENGTH == 0 and self.input_checksum is not None:
-            if self.input_checksum == byte:
-                self.emit_sys_code(_SYS_CODE_MSG_ACK)
-            else:
-                self.emit_sys_code(_SYS_CODE_MSG_ERR)
             self.input_checksum = 0xFF
             return
-        if self.input_checksum is None:
-            self.input_checksum = 0xFF
         self.input_checksum ^= byte
         self.input_buffer.append(byte)
-
-    def update(self, delta):
-        self.device.update(delta)
-        self.send_data_queue()
 
     def run_loop(self, max_delta):
         loop_watch = StopWatch()
@@ -104,4 +127,4 @@ class IOHub:
             t = loop_watch.time()
             delta = (t-last_time)/1000
             last_time = t
-            self.update(delta)
+            self.device.update(delta)
