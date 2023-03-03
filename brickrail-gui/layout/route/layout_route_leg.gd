9370e3daacae6ca1340df2d4be6a341c9ff1c69d
Johannes Ringler <johannesringler@gmail.com>
2023-03-03 17:16:44 +0100

    fix switches set incorrectly right before block
    fixes #58

--- brickrail-gui/layout/route/layout_route_leg.gd
+++ brickrail-gui/layout/route/layout_route_leg.gd
@@ -109,18 +109,15 @@ func lock_and_switch(trainname):
 func set_switches():
 	var last_track = get_start_node().obj.section.tracks[-1]
 	var forward_switches = []
-	for edge in edges:
-		if edge.section == null:
-			continue
-		for dirtrack in edge.section.tracks:
-			var forward_switch = last_track.get_switch()
-			if forward_switch != null:
-				forward_switches.append(forward_switch)
-				forward_switch.switch(dirtrack.get_turn())
-			var backward_switch = dirtrack.get_opposite().get_switch()
-			if backward_switch != null and not backward_switch in forward_switches:
-				backward_switch.switch(last_track.get_opposite().get_turn())
-			last_track = dirtrack
+	for dirtrack in get_section(false).tracks:
+		var forward_switch = last_track.get_switch()
+		if forward_switch != null:
+			forward_switches.append(forward_switch)
+			forward_switch.switch(dirtrack.get_turn())
+		var backward_switch = dirtrack.get_opposite().get_switch()
+		if backward_switch != null and not backward_switch in forward_switches:
+			backward_switch.switch(last_track.get_opposite().get_turn())
+		last_track = dirtrack
 
 func lock_tracks(trainname):
 	var lock_trains = get_lock_trains()
