Novakasa/brickrail

commit 988568ff94423a8b62b31991d8b618f7ad6786d6

fix ble and layout switch mismatch
when ble switch was already correct, layoutswitch wouldn't actually
switch to the new position

fixes #67

brickrail-gui/layout/switch/layout_switch.gd
@@ -129,14 +129,13 @@ func switch(pos):
 			#check only here in case ble switch position is unknown
 			pass
 		var motor_pos = pos_to_dev1(pos)
-		if motor_pos == motor1.position:
-			return
-		prints("switching motor1:", motor_pos)
-		motor1.switch(motor_pos)
-		position_index = switch_positions.find(pos)
-	else:
+		if motor_pos != motor1.position:
+			prints("switching motor1:", motor_pos)
+			motor1.switch(motor_pos)
 		position_index = switch_positions.find(pos)
 		emit_signal("position_changed", pos)
+	else:
+		position_index = switch_positions.find(pos)
 
 func get_position():
 	return switch_positions[position_index]

commit 043593882b0eaf08393bb641c5192ab3a10b2fff

fix switch position render regression
was introduced in 988568ff94423a8b62b31991d8b618f7ad6786d6

closes #89

brickrail-gui/layout/switch/layout_switch.gd
@@ -133,9 +133,9 @@ func switch(pos):
 			prints("switching motor1:", motor_pos)
 			motor1.switch(motor_pos)
 		position_index = switch_positions.find(pos)
-		emit_signal("position_changed", pos)
 	else:
 		position_index = switch_positions.find(pos)
+	emit_signal("position_changed", pos)
 
 func get_position():
 	return switch_positions[position_index]

brickrail-gui/tools/async_window.gd
@@ -22,5 +22,4 @@ func get_user_action_coroutine():
 	popup_centered()
 	var action = yield(self, "action_button_pressed")
 	hide()
-	print(action)
 	return action